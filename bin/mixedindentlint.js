daa5e701a036bdd6c324f4379e22d3b58f97ad9d
Payton Swick <payton@foolord.com>
2015-12-10 17:29:41 -0500

    Add --spaces and --tabs options to CLI

--- bin/mixedindentlint.js
+++ bin/mixedindentlint.js
@@ -4,7 +4,18 @@ var fs = require( 'fs' );
 var parseArgs = require( 'minimist' );
 var IndentChecker = require( '../lib/indent-checker' );
 
-var argv = parseArgs( process.argv.slice( 2 ) );
+var argv = parseArgs( process.argv.slice( 2 ), {
+	boolean: [
+		'version',
+		'spaces',
+		'tabs'
+	],
+	default: {
+		version: null,
+		spaces: null,
+		tabs: null
+	}
+} );
 var files = argv._;
 
 if ( argv.version ) {
@@ -18,6 +29,8 @@ if ( files.length < 1 ) {
 	console.log( 'Usage: mixedindentlint [options] <file1> [<file2>...]' );
 	console.log( 'Options:' );
 	console.log( '  --version\t\tPrint the current version.' );
+	console.log( '  --spaces\t\tAssume all files should use spaces rather than the most common indentation.' );
+	console.log( '  --tabs\t\tAssume all files should use tabs rather than the most common indentation.' );
 	process.exit( 0 );
 }
 
@@ -28,14 +41,28 @@ var messages = files.reduce( function( warnings, file ) {
 		console.error( 'Error trying to read file "' + file + '":', err.message );
 		process.exit( 1 );
 	}
-	var lines = IndentChecker.getLinesWithLessCommonType( input );
+	var lintOptions = {
+		indent: null
+	};
+	if ( argv.spaces ) {
+		lintOptions.indent = 'spaces';
+	} else if ( argv.tabs ) {
+		lintOptions.indent = 'tabs';
+	}
+	var lines = IndentChecker.lint( input, lintOptions );
 	if ( lines.length > 0 ) warnings[ file ] = lines;
 	return warnings;
 }, {} );
 
+var indentationType = 'the most common type';
+if ( argv.spaces ) {
+	indentationType = 'spaces';
+} else if ( argv.tabs ) {
+	indentationType = 'tabs';
+}
 Object.keys( messages ).map( function( file ) {
 	messages[ file ].map( function( line ) {
-		console.log( 'Line ' + line + ' in "' + file + '" has indentation that differs from the rest of the file.' );
+		console.log( 'Line ' + line + ' in "' + file + '" has indentation that differs from ' + indentationType + '.' );
 	} );
 } );
 
